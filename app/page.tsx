3cdc79e7ad5075d6edcb81e46ef93c9548bfb8aa
Christopher Hilton <kool1160@gmail.com>
2026-05-06 10:31:13 -0400

    M23 show splash before app shell

--- app/page.tsx
+++ app/page.tsx
@@ -1,5 +1,10 @@
 import { WocApp } from '../features/woc/components/WocApp';
+import { LaunchSplash } from '../features/woc/components/LaunchSplash';
 
 export default function Home() {
-  return <WocApp />;
+  return (
+    <LaunchSplash>
+      <WocApp />
+    </LaunchSplash>
+  );
 }
