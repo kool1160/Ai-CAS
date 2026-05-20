83ea1cd61a16681eb0f2e625aae3c4d29ebce934
Christopher Hilton <kool1160@gmail.com>
2026-05-20 13:44:56 -0400

    style: load Beta 2 supporting screens polish stylesheet

--- app/layout.tsx
+++ app/layout.tsx
@@ -3,6 +3,7 @@ import './globals.css';
 import './beta2-button-hierarchy.css';
 import './beta2-review-send-polish.css';
 import './beta2-capture-generate-polish.css';
+import './beta2-supporting-screens-polish.css';
 import './v2-more-admin-layout.css';
 import './v2-tablet-polish.css';
 
