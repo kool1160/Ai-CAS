e6a0ca0b7f8f03d0426899788ba25f5ab268d599
Christopher Hilton <kool1160@gmail.com>
2026-05-20 13:33:42 -0400

    style: load Beta 2 capture generate polish stylesheet

--- app/layout.tsx
+++ app/layout.tsx
@@ -2,6 +2,7 @@ import type { Metadata, Viewport } from 'next';
 import './globals.css';
 import './beta2-button-hierarchy.css';
 import './beta2-review-send-polish.css';
+import './beta2-capture-generate-polish.css';
 import './v2-more-admin-layout.css';
 import './v2-tablet-polish.css';
 
